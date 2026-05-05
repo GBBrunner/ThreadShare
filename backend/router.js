d98e66e5b3f57146eeac6297589c8236925f8f9d
GBBrunner <gbbrunner64@gmail.com>
2026-05-05 00:50:31 -0600

    removed legacy routes from router

--- backend/router.js
+++ backend/router.js
@@ -4,21 +4,11 @@ const router = express.Router();
 // Basically any route that will need to fetch or post data to the databse
 const loginRouter = require('./routes/login');
 const signupRouter = require('./routes/signup');
-const deleteAccountRouter = require('./routes/delete_account');
-const createCourseRouter = require('./routes/Create_Course');
-const coursesRouter = require('./routes/courses');
-const EnrollCourseRouter = require('./routes/Enroll_Course');
-const myCoursesRouter = require('./routes/My_Courses');
-const ViewStudentsRouter = require('./routes/View_Students');
+// const deleteAccountRouter = require('./routes/delete_account');
 
-router.use('/api', ViewStudentsRouter);
 router.use('/api', loginRouter);
 router.use('/api', signupRouter);
-router.use('/api', deleteAccountRouter);
-router.use('/api', createCourseRouter);
-router.use('/api', coursesRouter);
-router.use('/api', EnrollCourseRouter);
-router.use('/api', myCoursesRouter);
+// router.use('/api', deleteAccountRouter);
 
 module.exports = router;
 
