dfbb085ebe523c80801fc7e85ce01dfd006bc1fe
GBBrunner <gbbrunner64@gmail.com>
2026-05-05 13:15:40 -0600

    -rm legacy #9

--- PostgreSQL/schemas/users.sql
+++ PostgreSQL/schemas/users.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users (
     user_id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name    VARCHAR(20),
-    last_name     VARCHAR(20),
-	username     VARCHAR(20),
+    last_name     VARCHAR(20), 
+	username      VARCHAR(20), NOT NULL UNIQUE,
     email         VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL
 );
--- WebDevelopment/PostgreSQL/schemas/Add_courses _to_students.sql
+++
@@ -1,9 +0,0 @@
-SELECT setval('user_count', 100030);
-SELECT * from courses
-
-select * from users
-
-select * from student_courses
-
-INSERT INTO student_courses (user_id, course_id)
-VALUES ('550e8400-e29b-41d4-a716-446655440003', 'b18385a4-85ea-4dd7-871a-1ba53dfe7cdc');
