e6e7dca98f66bdab5e1f7702848573eaffb08ab2
GBBrunner <gbbrunner64@gmail.com>
2026-05-05 01:09:16 -0600

    -rm legacy #7

--- WebDevelopment/client/app/Enroll/page.jsx
+++
@@ -1,5 +0,0 @@
-'use client';
-import ViewCourses from "@/app/components/ViewCourses";
-export default function Enroll() {
-    return <ViewCourses method="POST" endpoint="/api/Enroll_Course" />;
-}
